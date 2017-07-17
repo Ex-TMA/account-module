4b571db00c44456b33bcb269bc87180977e9f40e
nsonanh <sonanhandynguyen@gmail.com>
2017-07-17 22:53:39 +0700

    [feature-gradle-setup] Set up gradle + spring boot

---
+++ src/main/java/security/Application.java
@@ -0,0 +1,15 @@
+package security;
+
+/**
+ * Created by nsonanh on 17/07/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
