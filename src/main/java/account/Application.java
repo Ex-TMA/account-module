bede693f8f9a728c2945c26625bba08f03f2db62
nsonanh <sonanhandynguyen@gmail.com>
2017-07-25 23:34:02 +0700

    [feature-reflection-update] Update Utils to use class name defined in properties file. Exclude auto configuration of spring security

--- src/main/java/account/Application.java
+++ src/main/java/account/Application.java
@@ -3,13 +3,13 @@ package account;
 /**
  * Created by nsonanh on 17/07/2017.
  */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
